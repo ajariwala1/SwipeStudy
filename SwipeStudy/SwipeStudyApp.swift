d7b9fc07553c6a4778bf5b517b38256512e0dbad
Abhishek Jariwala <ajariwala1@auburn.edu>
2026-06-25 09:43:02 -0700

    Initial Commit

---
+++ SwipeStudy/SwipeStudyApp.swift
@@ -0,0 +1,17 @@
+//
+// SwipeStudyApp.swift : SwipeStudy
+//
+// Copyright © 2026 Auburn University.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct SwipeStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
